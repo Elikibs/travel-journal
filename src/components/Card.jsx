481c871e5d4dbd4123759b79131101fc61bad7ad
Elisha <elishakibet67@gmail.com>
2023-11-19 13:34:01 +0300

    updated card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,19 @@
 function Card(){
     return (
-        <div>Card here</div>
+        <div className="card">
+            <img src="" alt="" />
+            <div className="card--location">
+                <img src="" alt="" className="location--image"/>
+                <span></span>
+                <span className="gray"></span>
+            </div>
+            <h3 className="card--title"></h3>
+            <div className="card--dates">
+                <p></p>
+                <span></span>
+            </div>
+            <p className="card--description"></p>
+        </div>
     )
 }
 
