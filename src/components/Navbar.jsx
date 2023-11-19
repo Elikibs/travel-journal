5d71c6244392179d20b8abb2aaea1f427c521b19
Elisha <elishakibet67@gmail.com>
2023-11-19 12:16:06 +0300

    added journal-logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ function Navbar(){
     return (
         <div>
             <nav>
+                <img src="/images/journal-logo.jpg" alt="journal-logo" />
                 <h3>My travel journal</h3>
             </nav>
         </div>
