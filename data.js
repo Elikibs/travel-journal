bceaacb80b3015737938eea1409d80721bc800a9
Elisha <elishakibet67@gmail.com>
2023-11-19 13:33:49 +0300

    added  some data

---
+++ data.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "3 Dec, 2023",
+        endDate: "10 Dec, 2023",
+        description: "Hello",
+        imageUrl: ""
+    }
+]
