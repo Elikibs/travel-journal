1b00610477297b26802ed13f9157b5ce76f7b246
Elisha <elishakibet67@gmail.com>
2023-11-19 13:46:42 +0300

    Added some data

--- data.js
+++ data.js
@@ -6,6 +6,15 @@ export default [
         startDate: "3 Dec, 2023",
         endDate: "10 Dec, 2023",
         description: "Hello",
-        imageUrl: ""
+        imageUrl: "https://unsplash.com/photos/a-large-snow-covered-mountain-towering-over-a-city-UVWJwEroPmo"
+    },
+    {
+        title: "Cape Town",
+        location: "South Africa",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "3 Jan, 2024",
+        endDate: "17 Jan, 2024",
+        description: "Hello",
+        imageUrl: "https://unsplash.com/photos/aerial-view-of-city-near-mountain-during-daytime-kpdWINVf3JI"
     }
 ]
