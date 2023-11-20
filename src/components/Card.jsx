262fe83274e4d8c9bf88c98bec40cce5e761c1b1
Elisha <elishakibet67@gmail.com>
2023-11-20 11:06:15 +0300

    Updated card structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,19 @@
 function Card(props){
     return (
         <div className="card">
-            <img src={props.imageUrl} alt="image-site" />
-            <div className="card--location">
-                <img src="/images/location-image.png" alt="" className="location--image"/>
-                <span>{props.location}</span>
-                <span className="gray">{props.googleMapsUrl}</span>
+            <div className="image-container">
+                <img src={props.imageUrl} alt="image-site" className="site--image" />
             </div>
-            <h3 className="card--title">{props.title}</h3>
-            <div className="card--dates">
-                <p>{props.startDate} - <span>{props.endDate}</span></p>
+            <div className="details-container">
+                <div className="card--location">
+                    <img src="/images/location-image.png" alt="" className="location--image"/>
+                    <span>{props.location}</span>
+                    <span><a href={props.googleMapsUrl} className="gray">Google maps link</a></span>
+                </div>
+                <h3 className="card--title">{props.title}</h3>
+                <p className="card--dates">{props.startDate} - <span>{props.endDate}</span></p>
+                <p className="card--description">{props.description}</p>
             </div>
-            <p className="card--description">{props.description}</p>
         </div>
     )
 }
