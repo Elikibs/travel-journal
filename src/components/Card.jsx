69fcfbee990e4d2d0f9527f0271287414e9f90a5
Elisha <elishakibet67@gmail.com>
2023-11-20 09:35:32 +0300

    corection on card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,13 @@ function Card(props){
         <div className="card">
             <img src={props.imageUrl} alt="image-site" />
             <div className="card--location">
-                <img src="" alt="" className="location--image"/>
+                <img src="/images/location-image.png" alt="" className="location--image"/>
                 <span>{props.location}</span>
                 <span className="gray">{props.googleMapsUrl}</span>
             </div>
             <h3 className="card--title">{props.title}</h3>
             <div className="card--dates">
-                <p>{props.startDate}</p>
-                <span>{props.endDate}</span>
+                <p>{props.startDate} - <span>{props.endDate}</span></p>
             </div>
             <p className="card--description">{props.description}</p>
         </div>
