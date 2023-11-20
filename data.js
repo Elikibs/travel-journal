e82b8a690a834b68074ba2804cc9e276301e0b4d
Elisha <elishakibet67@gmail.com>
2023-11-20 09:24:36 +0300

    Added location image

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ export default [
         startDate: "3 Dec, 2023",
         endDate: "10 Dec, 2023",
         description: "Hello",
-        imageUrl: "https://unsplash.com/photos/a-large-snow-covered-mountain-towering-over-a-city-UVWJwEroPmo"
+        imageUrl: "https://images.unsplash.com/photo-1578271887552-5ac3a72752bc?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         id: 2,
@@ -17,6 +17,16 @@ export default [
         startDate: "3 Jan, 2024",
         endDate: "17 Jan, 2024",
         description: "Hello",
-        imageUrl: "https://unsplash.com/photos/aerial-view-of-city-near-mountain-during-daytime-kpdWINVf3JI"
+        imageUrl: "https://images.unsplash.com/photo-1580060839134-75a5edca2e99?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 3,
+        title: "Miacle Garden",
+        location: "Dubai",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "1 Feb, 2024",
+        endDate: "14 Feb, 2024",
+        description: "Hello",
+        imageUrl: "https://images.unsplash.com/photo-1610208536976-bd40cbc8c940?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
 ]
