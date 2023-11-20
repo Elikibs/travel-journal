de7a28b964e11c9bfd2745ad664344d9b50133e6
Elisha <elishakibet67@gmail.com>
2023-11-20 09:35:48 +0300

    Added description to the data

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "3 Dec, 2023",
         endDate: "10 Dec, 2023",
-        description: "Hello",
+        description: "Mount Fuji is an active volcano, which most recently erupted in 1707. It stands on the border between Yamanashi and Shizuoka prefectures and can be seen from Tokyo and Yokohama on clear days. Another easy way to view Mount Fuji is from the train on a trip between Tokyo and Osaka.",
         imageUrl: "https://images.unsplash.com/photo-1578271887552-5ac3a72752bc?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
@@ -16,7 +16,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "3 Jan, 2024",
         endDate: "17 Jan, 2024",
-        description: "Hello",
+        description: "Cape Town is a modern, cosmopolitan city surrounded by nature. Centred around Table Mountain, one of the New 7 Wonders of the world, the Mother City is known for its beautiful beaches, stunning biodiversity, award-winning food and wine, and sheer variety of experiences on offer.",
         imageUrl: "https://images.unsplash.com/photo-1580060839134-75a5edca2e99?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
@@ -26,7 +26,7 @@ export default [
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "1 Feb, 2024",
         endDate: "14 Feb, 2024",
-        description: "Hello",
+        description: "Each season, as the weather begins to cool at the onset of winter, the gates reopen at Dubai Miracle Garden. With more than 150 million flowers in full bloom, this 72,000-sq-m paradise has flowered into one of the region's most picturesque, sweet-scented destinations for more than a decade now.",
         imageUrl: "https://images.unsplash.com/photo-1610208536976-bd40cbc8c940?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     }
 ]
