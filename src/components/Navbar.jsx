97fd9adab62e908f687cbee1975e51a35a3aca6e
Elisha <elishakibet67@gmail.com>
2023-11-20 11:39:51 +0300

    Added div class to nav element

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 function Navbar(){
     return (
-        <>
+        <div className="nav--div">
             <nav>
                 <img src="/images/journal-logo.jpg" alt="journal-logo" className="nav--logo"/>
                 <h3 className="title--text">My travel journal</h3>
             </nav>
-        </>
+        </div>
     )
 }
 
